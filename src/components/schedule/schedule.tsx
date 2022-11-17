ebbd7c34f1ae5f0d6077a6c61a81fe227af3f7e1
Thomas-Louis Simard <thomas.louis.simard@gmail.com>
2022-11-16 21:49:53 -0500

    fix(schedule): fix slider startIndex initialization
    - enable slider rewind option
    - enable slider trimSpace option
    - bump react-splide version to 0.6.0

--- src/components/schedule/schedule.tsx
+++ src/components/schedule/schedule.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useCallback, useRef, useState } from 'react';
+import React, { useEffect, useCallback, useRef, useState } from 'react';
 import { NHLScheduleModel } from '../../models/schedule';
 import ScheduleGames from './scheduleGames';
 import { Splide } from '@splidejs/react-splide';
@@ -16,8 +16,8 @@ type Props = {
 }
 
 const SLIDER_HIGH_SPEED_OCC_NUMBER_THRESHOLD = 2;
-const  SLIDER_LOW_TRANSITION_SPEED = 1500;
-const  SLIDER_HIGH_TRANSITION_SPEED = 200;
+const SLIDER_LOW_TRANSITION_SPEED = 1500;
+const SLIDER_HIGH_TRANSITION_SPEED = 200;
 
 const ScheduleGamesMemo = React.memo(ScheduleGames);
 
@@ -25,68 +25,70 @@ const Schedule: React.FC<Props> = ({ dates, loading, startIndex, onMoved }) => {
 
     const getSplideCount = () => Math.floor(window.innerWidth / 250);
     const slider = useRef<any>(null);
-    const [options, setOptions] = useState<any>({
+    const [options] = useState<any>({
         height: "180px",
+        start: startIndex ?? 0,
+        rewind: true,
+        rewindSpeed: SLIDER_HIGH_TRANSITION_SPEED,
+        drag: true,
         gap: "2px",
-        speed: getSplideCount() <= SLIDER_HIGH_SPEED_OCC_NUMBER_THRESHOLD ? 
-            SLIDER_HIGH_TRANSITION_SPEED : SLIDER_LOW_TRANSITION_SPEED,
         waitForTransition: false,
         autoWidth: true,
-        drag:  true,
         padding: { left: 2, right: 2 },
         pagination: false,
-        trimSpace: false,
+        trimSpace: true,
         perPage: getSplideCount(),
         perMove: getSplideCount()
     })
 
-    const resetSliderSpeed =  useCallback(debounce(() => {
+    const setSliderSpeed = useCallback(debounce(() => {
         if (slider.current) {
-            const occ = getSplideCount();
             const { splide } = slider.current;
-            splide.options.speed = occ <= SLIDER_HIGH_SPEED_OCC_NUMBER_THRESHOLD ? 
-            SLIDER_HIGH_TRANSITION_SPEED : SLIDER_LOW_TRANSITION_SPEED;
-            setOptions({ ...options, speed: splide.options.speed });
+            const occ = getSplideCount();
+            splide.options.speed = occ <= SLIDER_HIGH_SPEED_OCC_NUMBER_THRESHOLD ?
+                SLIDER_HIGH_TRANSITION_SPEED : SLIDER_LOW_TRANSITION_SPEED;
         }
-    }, SLIDER_HIGH_TRANSITION_SPEED, { leading: false, trailing: true }), []);
+        }, SLIDER_HIGH_TRANSITION_SPEED, { leading: false, trailing: true }), []);
+
+    const onWindowResize = useCallback(throttle(() => {
+        setSliderSpeed();
+        if (slider.current) {
+            const { splide } = slider.current;
+            const occ = getSplideCount();
+            if (splide.options.perPage !== occ) {
+                splide.options.perPage = occ;
+                splide.options.perMove = occ;
+            }
+        }
+    }, 200), []);
 
     useEffect(() => {
-        const handler = throttle(() => {
-            if (slider.current) {
-                const { splide } = slider.current;
-                const occ = getSplideCount();
-                if (splide.options.perPage !== occ) {
-                    splide.options.perPage = occ;
-                    splide.options.perMove = occ;
-                    splide.options.speed = occ <= SLIDER_HIGH_SPEED_OCC_NUMBER_THRESHOLD ? 
-                    SLIDER_HIGH_TRANSITION_SPEED : SLIDER_LOW_TRANSITION_SPEED;
-                    setOptions({ ...options, perPage: occ, perMove: occ, speed: splide.options.speed });
-                }
-            }
-        }, 200);
-        window.addEventListener("resize", handler);
+        setSliderSpeed();
+        window.addEventListener("resize", onWindowResize);
         return function cleanup(): void {
-            window.removeEventListener("resize", handler);
+            window.removeEventListener("resize", onWindowResize);
         }
-    }, [getSplideCount])
+    }, [])
 
-    useLayoutEffect(() => {
+    useEffect(() => {
         if (slider.current) {
+            const { splide } = slider.current;
             if (typeof (startIndex) !== "undefined" && slider.current.splide.index !== startIndex) {
-                setOptions({ ...options, start: startIndex });
-                slider.current.splide.options.start = startIndex;
-                slider.current.splide.refresh();
+                // always reset speed option, and set only once startIndex is set, and splide has been navigated to, to prevent transition delay.
+                splide.options.start = startIndex;
+                splide.options.speed = 0;
+                slider.current.splide.go(startIndex);
+                setSliderSpeed();
             }
         }
     }, [startIndex]);
 
-    const onDragEnd = () => resetSliderSpeed();
+    const onDragEnd = () => setSliderSpeed();
 
     const onDragStart = () => {
         const { splide } = slider.current;
         if (splide.options.speed !== SLIDER_HIGH_TRANSITION_SPEED && slider.current) {
             splide.options.speed = SLIDER_HIGH_TRANSITION_SPEED;
-            setOptions({ ...options, speed: splide.options.speed });
         }
     };
 
